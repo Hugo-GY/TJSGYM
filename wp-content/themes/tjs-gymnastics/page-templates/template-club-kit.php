11104b448a9361eb82340c34aa3d0dcc556dc372
jinzesudawei <jinzesudawei@163.com>
2026-04-06 21:21:41 +0800

    优化 Club Kit 页面

--- wp-content/themes/tjs-gymnastics/page-templates/template-club-kit.php
+++ wp-content/themes/tjs-gymnastics/page-templates/template-club-kit.php
@@ -68,16 +68,70 @@ $hero_title = get_field('hero_title') ?: 'Club <em>Kit</em>';
         
         <div class="kit-products-grid">
             <?php
-            $products = array(
-                array('name' => "TJ's T-Shirt", 'img' => '20889191', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/20889191/TJ-s-T-Shirt'),
-                array('name' => "TJ's T-Shirt - Kids", 'img' => '20889196', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/20889196/TJ-s-T-Shirt-Kids'),
-                array('name' => "TJ's Sweatshirt", 'img' => '20889181', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/20889181/TJ-s-Sweatshirt'),
-                array('name' => "TJ's Sweatshirt - Kids", 'img' => '20889186', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/20889186/TJ-s-Sweatshirt-Kids'),
-                array('name' => "TJ's Boys Shorts", 'img' => '20935746', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/20935746/TJ-s-Boys-Shorts'),
-                array('name' => "TJ's Girls Shorts", 'img' => '20935736', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/20935736/TJ-s-Girls-Shorts'),
-            );
+            // Query Club products
+            $club_query = new WP_Query(array(
+                'post_type' => 'post',
+                'posts_per_page' => -1,
+                'category_name' => 'club',
+                'orderby' => 'date',
+                'order' => 'ASC',
+            ));
             
-            foreach ($products as $product):
+            if ($club_query->have_posts()):
+                while ($club_query->have_posts()): $club_query->the_post();
+                    $product_name = get_the_title();
+                    $post_content = get_the_content();
+                    $product_url = get_post_meta(get_the_ID(), 'product_url', true);
+                    
+                    // Get image from post_content
+                    $product_image = '';
+                    if (preg_match('/<img[^>]+src=["\']([^"\']+)["\']/', $post_content, $matches)) {
+                        $product_image = $matches[1];
+                    }
+                    
+                    // Fallback to featured image if no image in content
+                    if (!$product_image) {
+                        $product_image = get_the_post_thumbnail_url(get_the_ID(), 'full');
+                    }
+                    
+                    // Fallback if no image found
+                    if (!$product_image) {
+                        $product_image = '';
+                    }
+            ?>
+                <a class="kit-product-card" href="<?php echo esc_url($product_url); ?>" target="_blank" rel="noopener">
+                    <div class="kit-product-image-wrap">
+                        <?php if ($product_image): ?>
+                            <img src="<?php echo esc_url($product_image); ?>" alt="<?php echo esc_attr($product_name); ?>" class="kit-product-image" loading="lazy">
+                        <?php endif; ?>
+                    </div>
+                    <h3><?php echo esc_html($product_name); ?></h3>
+                    <span class="kit-product-link"><?php _e('View Product', 'tjs-gymnastics'); ?></span>
+                </a>
+            <?php 
+                endwhile;
+                wp_reset_postdata();
+            else:
+                // Fallback static products if no dynamic posts found
+                $products = array(
+                    array('name' => "TJ's T-Shirt", 'img' => '20889191', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/20889191/TJ-s-T-Shirt'),
+                    array('name' => "TJ's T-Shirt - Kids", 'img' => '20889196', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/20889196/TJ-s-T-Shirt-Kids'),
+                    array('name' => "TJ's Sweatshirt", 'img' => '20889181', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/20889181/TJ-s-Sweatshirt'),
+                    array('name' => "TJ's Sweatshirt - Kids", 'img' => '20889186', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/20889186/TJ-s-Sweatshirt-Kids'),
+                    array('name' => "TJ's Boys Shorts", 'img' => '20935746', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/20935746/TJ-s-Boys-Shorts'),
+                    array('name' => "TJ's Girls Shorts", 'img' => '20935736', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/20935736/TJ-s-Girls-Shorts'),
+                    array('name' => "TJ's Performance Vest", 'img' => '20889176', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/20889176/TJ-s-Performance-Vest'),
+                    array('name' => "TJ's Vest", 'img' => '25594086', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/25594086/TJ-s-Vest'),
+                    array('name' => "TJ's Ladies Shorts", 'img' => '20927591', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/20927591/TJ-s-Ladies-Shorts'),
+                    array('name' => "TJ's Zip Sweatshirt", 'img' => '20927446', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/20927446/TJ-s-Zip-Sweatshirt'),
+                    array('name' => "TJ's Leggings", 'img' => '20927506', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/20927506/TJ-s-Leggings'),
+                    array('name' => "TJ's Capris", 'img' => '20927781', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/20927781/TJ-s-Capris'),
+                    array('name' => "TJ's Teddy Bear", 'img' => '21479806', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/21479806/TJ-s-Teddy-Bear'),
+                    array('name' => "TJ's Gymsac", 'img' => '20935851', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/20935851/TJ-s-Gymsac'),
+                    array('name' => "TJ's 2 Cap Bottle", 'img' => '20935816', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/20935816/TJ-s-2-Cap-Bottle'),
+                    array('name' => "TJ's Thermal Bottle", 'img' => '20935791', 'url' => 'https://tjs.printmykit.co.uk/shop/view_product/20935791/TJ-s-Thermal-Bottle'),
+                );
+                foreach ($products as $product):
             ?>
                 <a class="kit-product-card" href="<?php echo esc_url($product['url']); ?>" target="_blank" rel="noopener">
                     <div class="kit-product-image-wrap">
@@ -86,7 +140,7 @@ $hero_title = get_field('hero_title') ?: 'Club <em>Kit</em>';
                     <h3><?php echo esc_html($product['name']); ?></h3>
                     <span class="kit-product-link"><?php _e('View Product', 'tjs-gymnastics'); ?></span>
                 </a>
-            <?php endforeach; ?>
+            <?php endforeach; endif; ?>
         </div>
     </div>
 </section>
